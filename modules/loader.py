c17b56de8357484c25112797ff558552d544497a
janugupta534-bit <janugupta534@gmail.com>
2026-06-25 14:53:35 +0530

    Update loader.py

--- modules/loader.py
+++ modules/loader.py
@@ -1,23 +1,31 @@
+import streamlit as st
 import gspread
 import pandas as pd
 from google.oauth2.service_account import Credentials
 
 SHEET_ID = "18__D2Of5hKvidG40RQiAy_2gEDmayrlg9-0Mn97W8HE"
 
-SERVICE_ACCOUNT_FILE = "modules/study-analytics-dashboard-fab5a1f8904e.json"
-
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive",
 ]
 
+LOCAL_FILE = "modules/study-analytics-dashboard-fab5a1f8904e.json"
+
 
 def load_sheet():
 
-    credentials = Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE,
-        scopes=SCOPES,
-    )
+    try:
+        credentials = Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"],
+            scopes=SCOPES,
+        )
+
+    except Exception:
+        credentials = Credentials.from_service_account_file(
+            LOCAL_FILE,
+            scopes=SCOPES,
+        )
 
     client = gspread.authorize(credentials)
 
