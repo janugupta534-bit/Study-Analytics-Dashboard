e9f07f230041ee291620d6589666cc4e9b5f0ca9
janugupta534-bit <janugupta534@gmail.com>
2026-06-25 16:25:03 +0530

    Update charts.py

--- modules/charts.py
+++ modules/charts.py
@@ -1,7 +1,62 @@
 import streamlit as st
 import plotly.express as px
 
+is_single_student = df["Name"].nunique() == 1
+if is_single_student:
 
+    st.header("📚 Subject-wise Analysis")
+
+    subject_hours = (
+        df.groupby("Subject")["Hours"]
+        .sum()
+        .reset_index()
+        .sort_values("Hours", ascending=False)
+    )
+
+    fig1 = px.bar(
+        subject_hours,
+        x="Subject",
+        y="Hours",
+        color="Subject",
+        text_auto=".1f",
+        title="📖 Subject-wise Study Hours"
+    )
+
+    st.plotly_chart(fig1, use_container_width=True)
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+
+        fig2 = px.pie(
+            subject_hours,
+            names="Subject",
+            values="Hours",
+            hole=0.5,
+            title="📊 Subject Distribution"
+        )
+
+        st.plotly_chart(fig2, use_container_width=True)
+
+    with col2:
+
+        daily = (
+            df.groupby("Date")["Hours"]
+            .sum()
+            .reset_index()
+        )
+
+        fig3 = px.line(
+            daily,
+            x="Date",
+            y="Hours",
+            markers=True,
+            title="📈 Daily Study Trend"
+        )
+
+        st.plotly_chart(fig3, use_container_width=True)
+
+    return
 def show_charts(df):
 
     st.header("📊 Student Comparison Dashboard")
